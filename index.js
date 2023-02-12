2952dc1f9878df68953164ddc11e07bc18a31d95
Temuulen996 <temuulenuuree@gmail.com>
2023-02-12 17:10:06 +0800

    t

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const colors = require("colors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const asyncHandler = require("./middleware/asyncHandler");
+const errorHandler = require("./middleware/error");
 //
 dotenv.config({ path: "./config/config.env" });
 mongoose
@@ -22,20 +24,39 @@ mongoose
   });
 //models
 const Product = require("./models/Products");
+
 //
-app.get("/api/v1/products", async (req, res) => {
-  try {
+app.get(
+  "/api/v1/products",
+  asyncHandler(async (req, res) => {
     const data = await Product.find();
     res.status(200).send({ success: true, data: data });
-  } catch (err) {
-    res.status(200).send({ success: true, data: "hii" });
-  }
-});
-app.get("/", async (req, res) => {
-  res.status(200).send({ success: true, data: "success" });
-});
+  })
+);
+app.get(
+  "/api/v1/product/:id",
+  asyncHandler(async (req, res, next) => {
+    const id = req.params.id;
+    const data = await Product.findById(id);
+    res.status(200).send({ success: true, data: data });
+  })
+);
+app.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    res.status(200).send({ success: true, data: "success" });
+  })
+);
+app.use(errorHandler);
 //port, listen
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log(`server ${process.env.PORT} port дээр аслаа`.rainbow);
 });
 //
+//error automataar barij awah heseg
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`aldaa garlaa : ${err.message}`.red.underline.red.bold);
+  server.close(() => {
+    process.exit(1);
+  });
+});
